a985fbfd08e6e0d7148d52723bd4e43470438094
mohanapriyas1808 <143476042+mohanapriyas1808@users.noreply.github.com>
2024-09-10 18:38:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || "mysql",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "rootpassword",
-  database: process.env.DB_NAME || "studentDB"
+  database: process.env.DB_NAME || "userdb"
 });
 
 // Connect to MySQL with retry logic
